94ef3c18383798dae107247ef8278fd6f8ff9604
Noel Roy <noelroy96@gmail.com>
2017-01-23 21:37:55 +0530

    Profile Complete

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+# What is Peekstudy?
+
+Peekstudy is an application that let's you upload, store, and view all of your notes. You can now manage and view to your notes from any device, anywhere in the world.
--- peekstudy/urls.py
+++ peekstudy/urls.py
@@ -23,11 +23,27 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    #url(r'^$', core_views.home, name='home'),
+    url(r'^$', core_views.home, name='home'),
+
+#auth urls
     url(r'^login/', auth_views.login, {'template_name': 'core/cover.html'},
         name='login'),
     url(r'^logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^signup/$', peekstudy_auth_views.signup, name='signup'),
+
+#account urls
+    url(r'^settings/$', core_views.settings, name='settings'),
+    url(r'^settings/picture/$', core_views.picture, name='picture'),
+    url(r'^settings/upload_picture/$', core_views.upload_picture,
+        name='upload_picture'),
+    url(r'^settings/save_uploaded_picture/$', core_views.save_uploaded_picture,
+        name='save_uploaded_picture'),
+    url(r'^settings/password/$', core_views.password, name='password'),
+
+#profile url
+    url(r'^(?P<username>[^/]+)/$', core_views.profile, name='profile'),
+
+
 ]
 
 if settings.DEBUG:
